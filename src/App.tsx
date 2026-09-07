6d951c381e80cadf48c1881059101df9dda8c18b
Henrique Akira Komati <h.akirakomati@gmail.com>
2026-09-07 16:41:28 -0300

    feat: Adição de tela Fontes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Componentes from "./pages/Componentes";
+import Fontes from "./pages/Fontes";
 import Inicio from "./pages/Inicio";
+import Metodologia from "./pages/Metodologia";
 import Pesquisa from "./pages/Pesquisa";
 import Produto from "./pages/Produto";
+import Sobre from "./pages/Sobre";
 import NaoEncontrado from "./pages/NaoEncontrado";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
       <Route path="/componentes" element={<Componentes />} />
       <Route path="/pesquisa" element={<Pesquisa />} />
       <Route path="/produto/:slug" element={<Produto />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/metodologia" element={<Metodologia />} />
+      <Route path="/fontes" element={<Fontes />} />
       <Route path="*" element={<NaoEncontrado />} />
     </Routes>
   );
